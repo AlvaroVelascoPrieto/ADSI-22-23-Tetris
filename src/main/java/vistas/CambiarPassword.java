fc47ce4d7604163d252c7d72b3bc9bda8c49d5dc
Reche20 <91413674+Reche20@users.noreply.github.com>
2022-12-11 23:56:38 +0100

    Todas las vistas de un usuario normal añadidas(Faltan las de ADMIN)

---
+++ src/main/java/vistas/CambiarPassword.java
@@ -0,0 +1,62 @@
+package vistas;
+
+import java.awt.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.*;
+
+public class CambiarPassword extends JFrame{
+
+    private JPanel contentPane;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    CambiarPassword frame = new CambiarPassword();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+    public CambiarPassword(){
+
+    setTitle("Cambiar Contraseña");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setBounds(100, 100, 450, 300);
+    contentPane = new JPanel();
+    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+    setContentPane(contentPane);
+    contentPane.setLayout(null);
+
+
+    //Nueva contraseña
+    JLabel newPasswordLabel = new JLabel();
+    newPasswordLabel.setText("Nueva Contraseña: ");
+    newPasswordLabel.setBounds(152, 40, 200, 23);
+    contentPane.add(newPasswordLabel);
+
+    JTextField textoNewPassword = new JTextField();
+    textoNewPassword.setBounds(152,62,140,23);
+    contentPane.add(textoNewPassword);
+
+
+    //Botones
+
+    JButton continuarbtn = new JButton();
+    continuarbtn.setText("Continuar");
+    continuarbtn.setBounds(170,93,100,23);
+    contentPane.add(continuarbtn);
+
+
+    JButton atrasbtn = new JButton();
+    atrasbtn.setText("Atras");
+    atrasbtn.setBounds(170,127,100,23);
+    contentPane.add(atrasbtn);
+            
+    }
+    
+}
