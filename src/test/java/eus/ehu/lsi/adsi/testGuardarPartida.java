0dca92bf8e43b0c10cf33791b369c3fb2f86acae
Hugo <Hugo@PC-HUGO>
2022-12-17 19:02:14 +0100

    Junits de guardar partida empezados

---
+++ src/test/java/eus/ehu/lsi/adsi/testGuardarPartida.java
@@ -0,0 +1,18 @@
+package eus.ehu.lsi.adsi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testGuardarPartida {
+
+	@Test
+	public void testGuardarPartida() {
+		fail("Not yet implemented");
+	}
+	
+	public void testCargarPartida(){
+		fail("Not yet implemented");
+	}
+
+}
